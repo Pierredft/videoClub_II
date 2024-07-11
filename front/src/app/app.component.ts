98c134773f1336440ba5f51b2779e702cf8051b5
pierre defauquet <pierredefauquet@gmail.com>
2024-07-11 13:25:39 +0200

    maj#2 configuration du fichier app.component.html et ts + création et config commons navBar et notFound

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./commons/nav-bar/nav-bar.component";
+import { HomeComponent } from "./pages/home/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
